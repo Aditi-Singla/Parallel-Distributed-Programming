a7a376c399bd0aa126c0feab858afc79912ecd93
Aditi-Singla <aditisksingla@gmail.com>
2019-01-24 15:42:08 +0530

    Add scripts and makefiles

---
+++ Lab1/scripts/run.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [ -f par ]; then
+	if [ "$#" == 2 ]; then
+        ./par "$1" "$2"
+    elif [ "$#" == 4 ] && [ "$3" == "-numThreads" ]; then
+        ./par "$1" "$2" "$4"
+    else
+        echo "Illegal number of parameters ($#)"
+    fi
+else
+    echo "Executable not found, run compile.sh first"
+fi
